4fd4e59422109c424e94c5089b859f28004a93cf
niyao <yao.ni@yintech.cn>
2019-03-28 14:42:29 +0800

    Feature: example project for NYVideoKit

--- Example/NYVideoKit/NYViewController.m
+++ Example/NYVideoKit/NYViewController.m
@@ -8,22 +8,85 @@
 
 #import "NYViewController.h"
 
+#import <NYVideoRecorder.h>
+#import <NYVideoKitDefinition.h>
+
 @interface NYViewController ()
+
+@property (nonatomic, strong) UIButton *reloadButton;
+@property (nonatomic, strong) NYVideoRecorder *recorder;
 
 @end
 
 @implementation NYViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self.view.layer addSublayer:self.recorder.videoPreviewLayer];
+    [self.view addSubview:self.reloadButton];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.recorder startCapture];
 }
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    [self.recorder stopCapture];
+}
+
+- (NYVideoRecorder *)recorder {
+    if (!_recorder) {
+        _recorder = [[NYVideoRecorder alloc] init];
+        [_recorder setPreviewLayerFrame:self.view.frame];
+    }
+    return _recorder;
+}
+
+- (WKWebView *)webView {
+    if (!_webView) {
+        WKWebViewConfiguration * configuration = [[WKWebViewConfiguration alloc]init];
+        configuration.allowsInlineMediaPlayback = YES;
+        configuration.websiteDataStore = [WKWebsiteDataStore defaultDataStore];
+        _webView = [[WKWebView alloc] initWithFrame:self.view.frame configuration:configuration];
+        _webView.navigationDelegate = self;
+        _webView.scrollView.delegate = self;
+        if (@available(iOS 11.0, *)) {
+            _webView.scrollView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+        } else {
+            
+        }
+        [self.view addSubview:_webView];
+    }
+    return _webView;
+}
+
+- (UIButton *)reloadButton {
+    if (!_reloadButton) {
+        _reloadButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        _reloadButton.frame = CGRectMake(self.view.frame.size.width - 100, self.view.frame.size.height - 50, 100, 30);
+        [_reloadButton setTitleColor:UIColor.blueColor forState:UIControlStateNormal];
+        [_reloadButton setTitle:@"⏺️" forState:UIControlStateNormal];
+        [_reloadButton addTarget:self action:@selector(startRecord) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _reloadButton;
+}
+
+- (void)startRecord {
+    if(self.recorder.recorderStatus != NYVideoRecorderStatusWriting) {
+        [self.recorder startRecording];
+        [self.reloadButton setTitle:@"⏸️" forState:UIControlStateNormal];
+    } else {
+        __weak typeof (self) weakSelf = self;
+        [self.recorder stopRecording:^(NSError * _Nullable error) {
+            if (error) {
+                [self showMessage:error.localizedDescription];
+            }
+            [weakSelf.reloadButton setTitle:@"⏺️" forState:UIControlStateNormal];
+        }];
+    }
+}
+
 
 @end
